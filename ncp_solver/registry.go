e33cb5afd57f0560431d6a3e2907a1480326bdbd
Bevan Rudge <bevan.rudge@tixtrack.com>
2025-05-12 20:12:40 +1200

    find duplicate solutions

--- ncp_solver/registry.go
+++ ncp_solver/registry.go
@@ -14,35 +14,36 @@ type Registry map[Solution]bool
 
 func (r Registry) add(solution, orientations [9]int) {
 	key := Solution{solution, orientations}
-	found, ok := r[key]
-	if ok && found {
-		panic(fmt.Sprintf("solution already exists: %v", key))
-	} else {
+	_, ok := r[key]
+	if !ok {
 		r[key] = true
-		if !ok {
-			for range 3 {
-				// Deduplicate solutions.
-				// TODO Rotate 90º and get new key.
-				// Once we're confident that each unique solution is found exactly four times, simplify the algorithm;
-				// Never rotate position 0.
-				r[key] = false
-			}
+		for range 3 {
+			// Deduplicate solutions.
+			// TODO Rotate 90º and get new key.
+			// Once we're confident that each unique solution is found exactly four times, simplify the algorithm;
+			// Never rotate position 0.
+			key = key.rotate()
+			r[key] = false
 		}
 	}
 }
 
 func (r Registry) format() string {
-	lines := []string{
-		fmt.Sprintf("Solutions found: %d", len(r)),
+	solutions := []string{}
+	for solution, unique := range r {
+		if unique {
+			solutions = append(solutions, solution.format())
+		}
+	}
+	result := []string{
+		fmt.Sprintf("Solutions found: %d", len(solutions)),
 		"",
 		"Tile number by position, including the orientation of North/top edge of tile",
 		"",
 		"P: T O",
 	}
-	for solution := range r {
-		lines = append(lines, solution.format())
-	}
-	return strings.Join(lines, "\n")
+	result = append(result, solutions...)
+	return strings.Join(result, "\n")
 }
 
 func (s *Solution) format() string {
@@ -54,4 +55,20 @@ func (s *Solution) format() string {
 		lines = append(lines, fmt.Sprintf("%d: %d %c", position, tile+1, orientation))
 	}
 	return strings.Join(lines, "\n")
+}
+
+func (s *Solution) rotate() Solution {
+	result := Solution{
+		solution: [9]int{
+			s.solution[6], s.solution[3], s.solution[0],
+			s.solution[7], s.solution[4], s.solution[1],
+			s.solution[8], s.solution[5], s.solution[2],
+		},
+	}
+
+	for tile, orientation := range s.orientations {
+		result.orientations[tile] = (orientation + 1) % 4
+	}
+
+	return result
 }
